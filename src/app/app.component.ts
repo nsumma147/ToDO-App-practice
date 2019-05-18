8c934e00d8bb88ff5e4f5785cdaa58a7dcff2be2
nsumma147 <nicholas.summa@gmail.com>
2019-05-18 14:24:05 -0500

    added css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,20 @@ export class AppComponent {
   title = 'todo-app practice';
   todoArray = [];
 
+  //add item
   addTodo(value) {
     this.todoArray.push(value)
     console.log(this.todoArray)
   }
+
+  //delete item
+  
+  deleteItem(todo){
+   for(let i=0 ;i<= this.todoArray.length ;i++){
+    if(todo== this.todoArray[i]){
+     this.todoArray.splice(i,1)
+    }
+   }
+  }
+
 }
